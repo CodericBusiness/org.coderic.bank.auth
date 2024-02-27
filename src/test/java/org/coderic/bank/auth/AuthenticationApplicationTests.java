1f38228eedb07c920e83c2d07d74cb7002afb244
Neftali Yagua <neftaliyagua@fedoraproject.org>
2024-02-26 21:16:44 -0500

    Fix test

--- src/test/java/org/coderic/bank/auth/AuthenticationApplicationTests.java
+++ src/test/java/org/coderic/bank/auth/AuthenticationApplicationTests.java
@@ -2,7 +2,10 @@ package org.coderic.bank.auth;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 
+@ContextConfiguration(loader = AnnotationConfigContextLoader.class)
 @SpringBootTest
 class AuthenticationApplicationTests {
 
