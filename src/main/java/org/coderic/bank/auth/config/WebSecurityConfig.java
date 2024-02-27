cea3217b1d44f6ecd36308aff4abe8522134ba33
Neftali Yagua <neftaliyagua@fedoraproject.org>
2024-02-26 21:22:35 -0500

    Fix test

--- src/main/java/org/coderic/bank/auth/config/WebSecurityConfig.java
+++ src/main/java/org/coderic/bank/auth/config/WebSecurityConfig.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.kerberos.authentication.KerberosAuthenticationProvider;
 import org.springframework.security.kerberos.authentication.sun.SunJaasKerberosClient;
 import org.springframework.security.kerberos.authentication.sun.SunJaasKerberosTicketValidator;
@@ -27,29 +29,32 @@ public class WebSecurityConfig extends AbstractHttpConfigurer<WebSecurityConfig,
     public void configure(HttpSecurity http) throws Exception {
 
         AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
-        http.addFilterBefore(spnegoAuthenticationProcessingFilter(authenticationManager),
-                BasicAuthenticationFilter.class);
+        http.addFilterBefore(spnegoAuthenticationProcessingFilter(authenticationManager), BasicAuthenticationFilter.class);
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated())
+                        .anyRequest().authenticated()
+                )
                 .formLogin(Customizer.withDefaults())
                 .exceptionHandling((exceptions) -> exceptions
-                        .defaultAuthenticationEntryPointFor(
-                                spnegoEntryPoint(),
-                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)));
+                .defaultAuthenticationEntryPointFor(
+                        spnegoEntryPoint(),
+                        new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                )
+        );
         http
                 .headers(headers -> headers
                         .httpStrictTransportSecurity(hsts -> hsts
                                 .includeSubDomains(true)
                                 .preload(false)
-                                .maxAgeInSeconds(40)));
+                                .maxAgeInSeconds(40)
+                        )
+                );
         return http.build();
     }
-
     @Bean
     public AuthenticationManager authManager(HttpSecurity http) throws Exception {
         return http
@@ -74,8 +79,7 @@ public class WebSecurityConfig extends AbstractHttpConfigurer<WebSecurityConfig,
     }
 
     @Bean
-    public SpnegoAuthenticationProcessingFilter spnegoAuthenticationProcessingFilter(
-            AuthenticationManager authenticationManager) {
+    public SpnegoAuthenticationProcessingFilter spnegoAuthenticationProcessingFilter(AuthenticationManager authenticationManager) {
         SpnegoAuthenticationProcessingFilter filter = new SpnegoAuthenticationProcessingFilter();
         filter.setAuthenticationManager(authenticationManager);
         return filter;
