c8dcf703331a644f74b94aab10433d9f4a9e0845
Neftali Yagua <neftaliyagua@fedoraproject.org>
2024-03-04 20:51:49 -0500

    Add Scan extention

--- src/test/java/org/coderic/bank/auth/AuthenticationApplicationTests.java
+++ src/test/java/org/coderic/bank/auth/AuthenticationApplicationTests.java
@@ -1,21 +1,10 @@
 package org.coderic.bank.auth;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.kerberos.client.KerberosRestTemplate;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
-import org.springframework.util.Assert;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
-@WithMockUser(username="admin",password="dsdsddd")
 class AuthenticationApplicationTests {
 
 	@Test
